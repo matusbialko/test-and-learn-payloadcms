ec2cad7cd057f9a116cfa956d74f4511896d924e
Matus B <bialkomatus@gmail.com>
2025-09-26 15:19:39 +0200

    Add Hero45 to heroes

--- src/heros/RenderHero.tsx
+++ src/heros/RenderHero.tsx
@@ -5,11 +5,13 @@ import type { Page } from '@/payload-types'
 import { HighImpactHero } from '@/heros/HighImpact'
 import { LowImpactHero } from '@/heros/LowImpact'
 import { MediumImpactHero } from '@/heros/MediumImpact'
+import { Hero45 } from '@/heros/Hero45'
 
 const heroes = {
   highImpact: HighImpactHero,
   lowImpact: LowImpactHero,
   mediumImpact: MediumImpactHero,
+  hero45: Hero45,
 }
 
 export const RenderHero: React.FC<Page['hero']> = (props) => {
--- src/heros/config.ts
+++ src/heros/config.ts
@@ -35,6 +35,10 @@ export const hero: Field = {
           label: 'Low Impact',
           value: 'lowImpact',
         },
+        {
+          label: 'Hero 45',
+          value: 'hero45',
+        },
       ],
       required: true,
     },
@@ -62,7 +66,7 @@ export const hero: Field = {
       name: 'media',
       type: 'upload',
       admin: {
-        condition: (_, { type } = {}) => ['highImpact', 'mediumImpact'].includes(type),
+        condition: (_, { type } = {}) => ['highImpact', 'mediumImpact', 'hero45'].includes(type),
       },
       relationTo: 'media',
       required: true,
